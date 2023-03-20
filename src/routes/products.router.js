f83799da818f90ee00b2748a58a2cd58d735ef48
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-19 21:44:05 -0500

    adding try/catch to the router

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -6,8 +6,18 @@ const productsRouter = Router();
 const services = new ProductsService();
 
 productsRouter.get('/', (req, res) => {
-  const products = services.find();
-  res.json({length: products.length, products});
+  try {
+    const products = services.find();
+    res.status(200).json({
+      length: products.length,
+      products
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: 'Bad request',
+      error
+    });
+  }
 });
 
 productsRouter.get('/filter', (req, res) => {
@@ -16,28 +26,45 @@ productsRouter.get('/filter', (req, res) => {
 
 productsRouter.get('/:id', (req, res) => {
   const { id } = req.params;
-  const product = services.findOne(id);
 
-  if (product) {
-    return res.status(200).json(product);
+  try {
+    const product = services.findOne(id);
+    if (product) return res.status(200).json(product);
+    res.status(404).json({ message: 'Not found' });
+  } catch (error) {
+    res.status(400).send({error});
   }
-  res.status(404).json({
-    message: 'Not found'
-  });
 });
 
 productsRouter.post('/', (req, res) => {
   const { body } = req;
-  const newProduct = services.create(body);
-  res.status(201).json(newProduct);
+
+  try {
+    const newProduct = services.create(body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).send({
+      message: 'ocurrió un error en el registro',
+      error
+    });
+  }
 });
 
 productsRouter.patch('/:id', (req, res) => {
   const { id } = req.params;
   const { body } = req;
-  const updated = services.update(id, { ...body });
-
-  res.json(updated);
+  try {
+    const updated = services.update(id, body);
+    res.status(201).json({
+      message: 'Updated',
+      data: updated
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: 'ocurrió un error en el registro',
+      error
+    });
+  }
 });
 
 productsRouter.delete('/:id', async (req, res) => {
@@ -45,7 +72,10 @@ productsRouter.delete('/:id', async (req, res) => {
 
   try {
     const deleted = services.delete(id);
-    res.status(204).json(deleted);
+    res.status(204).json({
+      message: 'deleted',
+      deleted
+    });
   } catch (error) {
     res.status(404).json(error);
   }
