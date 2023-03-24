c8693655fbc09e37ab25d59b4bd97beaa25ec98e
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-24 17:51:18 -0500

    Adding isBlock property

--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -4,17 +4,20 @@ const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(25);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri();
+const isBlock = Joi.boolean();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
-  image: image.required()
+  image: image.required(),
+  isBlock: isBlock.required()
 });
 
 const updateProductSchema = Joi.object({
   name: name,
   price: price,
-  image: image
+  image: image,
+  isBlock: isBlock
 });
 
 const getProductSchema = Joi.object({
