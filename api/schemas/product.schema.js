9b84c0fcf52472cab1eef8688e0d25f83bb32a8d
carlosandf <carlos.andres.ff2912@gmail.com>
2023-04-09 15:01:53 -0500

    Addd new property

--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -1,16 +1,18 @@
 const Joi = require('joi');
 
-const id = Joi.string().uuid();
-const name = Joi.string().min(3).max(25);
-const price = Joi.number().integer().min(10);
-const image = Joi.string().uri();
-const isBlock = Joi.boolean();
+const id          = Joi.string().uuid();
+const name        = Joi.string().min(3).max(25);
+const price       = Joi.number().integer().min(10);
+const image       = Joi.string().uri();
+const isBlock     = Joi.boolean();
+const description = Joi.string();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
   image: image.required(),
-  isBlock: isBlock.required()
+  isBlock: isBlock.required(),
+  description: description.required()
 });
 
 const updateProductSchema = Joi.object({
